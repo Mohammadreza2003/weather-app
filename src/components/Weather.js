d42ee14588afaac21f94f402142925498aa014d9
Mohammadreza2003 <mohamadrezaahi2003@gmail.com>
2023-03-19 13:10:53 +0330

    I made the dark mode whith redux,added it to the app and version one is over

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from "axios"
 import './Weather.css';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import  dark  from '../them/dark';
 import light  from '../them/light';
